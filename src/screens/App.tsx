c600900e865f81c3db4cafb0e89f48da258c7d08
Artur Souza <artur.souza@liferay.com>
2023-12-28 17:55:21 -0300

    Add Header and Table components to App screen

--- src/screens/App.tsx
+++ src/screens/App.tsx
@@ -1,9 +1,41 @@
-function App() {
-  return (
-    <>
-        <h1>App</h1>
-    </>
-  )
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Header from "../components/Header";
+import Table from "../components/Table";
+
+interface IApp {
+  results: IAppData[];
 }
 
-export default App
+function App() {
+  const [data, setData] = useState<IApp>();
+  const [search, setSearch] = useState("");
+
+  const filteredData: IAppData[] =
+    data?.results.filter((result) =>
+      `${result.name.first} ${result.name.last}`
+        .toLowerCase()
+        .includes(search.toLowerCase())
+    ) || [];
+
+  const getData = () => {
+    axios
+      .get("http://localhost:5174/api/data")
+      .then((res) => setData(res.data))
+      .catch((err) => console.error(err));
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <div>
+      <Header onChange={(e) => setSearch(e.target.value)} value={search} />
+
+      <Table filteredData={filteredData} />
+    </div>
+  );
+}
+
+export default App;
