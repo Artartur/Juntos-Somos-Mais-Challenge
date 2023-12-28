273c7a336d11300fb3b8b62633001582a6cef95b
Artur Souza <artur.souza@liferay.com>
2023-12-28 17:53:09 -0300

    Add global interface

---
+++ src/utils/index.d.ts
@@ -0,0 +1,17 @@
+interface IAppData {
+    cell: string;
+    dob: { age: number; date: string };
+    email: string;
+    gender: string;
+    location: {
+      city: string;
+      coordinates: { latitude: string; longitude: string };
+      postcode: number;
+      state: string;
+      street: string;
+    };
+    name: { first: string; last: string; title: string };
+    picture: { medium: string };
+    registered: { date: string };
+  }
+  
